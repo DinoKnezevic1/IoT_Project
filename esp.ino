203808de3eb8d942c9db4c8c9d53be5b916888ab
Dino Knezevic <73589669+DinoKnezevic1@users.noreply.github.com>
2024-02-14 23:53:20 +0100

    Update esp.ino

---
+++ esp.ino
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi 
+const char* ssid = "bijeli64";
+const char* password = "ferit.2023";
+
+const char* mqtt_server = "192.168.25.190";
+const int mqtt_port = 1883;
+const char* mqtt_topic_validation = "sensors/validation";
+const char* mqtt_topic_keypad = "sensors/keypad";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      client.subscribe(mqtt_topic_validation);
+      client.subscribe(mqtt_topic_keypad);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
