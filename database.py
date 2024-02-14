8ec4f25bc764e05930478afab11818ab93c458f1
Dino Knezevic <73589669+DinoKnezevic1@users.noreply.github.com>
2024-02-14 23:54:14 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-import json
 import paho.mqtt.client as mqtt
 import time
 import MySQLdb as mdb
@@ -8,46 +7,66 @@ import MySQLdb as mdb
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# MQTT settings
+# MQTT 
 mqtt_broker_address = "localhost"
 mqtt_broker_port = 1883
-mqtt_topics = ["sensors/keypad"]
+mqtt_keypad_topic = "sensors/keypad"
+mqtt_validation_topic = "sensors/validation"
 
-# Initialize MQTT client
+# MQTT client
 mqtt_client = mqtt.Client()
-con = mdb.connect('localhost', 'sensor_writer', 'password', 'projekt')
+# Povezivanje na MySQL bazu podataka
+con = mdb.connect('localhost', 'rpi6', 'raspberrypi6', 'sifre_korisnika')
 cursor = con.cursor()
 
-# Define callback functions for MQTT
+# Varijabla za pohranu trenutne šifre koju korisnik unosi
+current_keypad_code = ""
+
+# Callback funkcija MQTT
 def on_connect(client, userdata, flags, rc):
     print(f"Connected to MQTT broker with result code {rc}")
-    for topic in mqtt_topics:
-        client.subscribe(topic)
+    client.subscribe(mqtt_keypad_topic)
 
 
 def on_message(client, userdata, msg):
+    global current_keypad_code
     topic = msg.topic
     payload = msg.payload.decode("utf-8")
     print(f"Received message on topic {topic}: {payload}")
-    # Emit the message to all connected WebSocket clients
-    socketio.emit("mqtt_message", {"topic": topic, "message": payload})
 
-    if topic == "sensors/keypad":
-        # Insert data into the database
-        sql = "INSERT INTO sensorData(datum, vrijeme, sifra) VALUES ('%s', '%s', '%s')" % (time.strftime("%Y-%m-%d"), time.strftime("%H:%M:%S"), payload)
-        cursor.execute(sql)
-        con.commit()
+    if topic == mqtt_keypad_topic:
+        # Objavite šifru na MQTT temu kada korisnik pritisne tipku #
+        if payload == "#":
+            if current_keypad_code:
+                mqtt_client.publish(mqtt_keypad_topic, current_keypad_code)
+                current_keypad_code = ""  # Resetirajte trenutnu šifru nakon što je objavljena
+            else:
+                print("No keypad code entered")
+        else:
+            # Ažurirajte trenutnu šifru kada korisnik unese broj
+            current_keypad_code += payload
+    else:
+        # Provjerite je li kod postoji u bazi podataka
+        cursor.execute("SELECT username FROM keypad_data WHERE sifra = '{}'".format(payload))
+        result = cursor.fetchone()
+        if result:
+            # Ako kod postoji, objavite korisničko ime zajedno s rezultatom validacije
+            validation_result = result[0]
+        else:
+            validation_result = 0
+        # Objavite rezultat validacije na MQTT temu
+        mqtt_client.publish(mqtt_validation_topic, str(validation_result), "1")
 
-# Set MQTT callback functions
+# Postavljanje MQTT callback funkcija
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 
-# Connect to MQTT broker
+
 mqtt_client.connect(mqtt_broker_address, mqtt_broker_port, 60)
 
 mqtt_client.loop_start()
 
-# Define the route for the HTML page
+# Ruta
 @app.route('/')
 def index():
     return render_template('index.html')
